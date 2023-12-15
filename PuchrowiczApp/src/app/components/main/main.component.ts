ccfeae1ffea845933eb475523c7dac4663f1543b
MarkulakS <markulaks17@gmail.com>
2023-12-15 18:41:55 +0100

    Changed animation on profile photo; style changes

--- PuchrowiczApp/src/app/components/main/main.component.ts
+++ PuchrowiczApp/src/app/components/main/main.component.ts
@@ -1,6 +1,4 @@
 import { Component, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { Router } from '@angular/router';
 import { EmailService } from 'src/app/services/email.service';
 
 @Component({
@@ -12,10 +10,8 @@ export class MainComponent implements OnInit {
   name: string = '';
   email: string = '';
   message: string = '';
-  isShining = false;
 
-  constructor(private router: Router, private formBuilder: FormBuilder, private emailService: EmailService,
-    private el: ElementRef, private renderer: Renderer2) { 
+  constructor(private emailService: EmailService, private el: ElementRef, private renderer: Renderer2) { 
   }
 
   ngOnInit(): void {
@@ -48,44 +44,4 @@ export class MainComponent implements OnInit {
       console.log(data);
     })
   }
-
-  addAnimation() {
-    this.isShining = true;
-
-    setTimeout(() => {
-      this.isShining = false;
-
-      // Odczekaj krótko, a następnie włącz animację ponownie
-      setTimeout(() => {
-        this.isShining = true;
-      }, 100);
-    }, 1000); // 2000 ms, czyli 2 sekundy
-  }
-
-  @HostListener('click', ['$event'])
-  onClick(event: Event) {
-    this.handleInteraction(event);
-  }
-
-  @HostListener('touchstart', ['$event'])
-  onTouchStart(event: Event) {
-    this.handleInteraction(event);
-  }
-
-  private handleInteraction(event: Event) {
-    const target = event.target as HTMLElement;
-
-    // Sprawdź, czy kliknięty element to ten, na którym chcesz uruchomić animację
-    if (target.matches('.shine-class')) {
-      this.addAnimation();
-    }
-    // Dodaj klasę na krótko w celu zresetowania pseudoelementu ::before
-    this.renderer.addClass(this.el.nativeElement, 'temporary-class');
-
-    // Usuń klasę po pewnym czasie
-    setTimeout(() => {
-      this.renderer.removeClass(this.el.nativeElement, 'temporary-class');
-    }, 100);
-  }
-
 }
